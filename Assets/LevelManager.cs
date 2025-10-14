d8285674e3bfb9b5045528d9721ba72c4403be57
vladignatovs <vi00242@rvt.lv>
2025-10-14 20:00:06 +0300

    ver update

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -86,7 +86,7 @@ public class LevelManager : MonoBehaviour {
 
         if(wReq.result != UnityWebRequest.Result.Success) {
             Debug.LogError("Failed to get levels: " + wReq.error);
-            // If can't connect, make sure that the server is running (USING MAMP)
+            // If can't connect, make sure that the server is running (USING MAMP) omfg DELETE THIS WHOLE THING
         }
         JsonData jsonData = JsonMapper.ToObject(wReq.downloadHandler.text);
         List<Level> levels = new();
